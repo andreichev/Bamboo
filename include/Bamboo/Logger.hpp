f3ccd4e9d5b56b5ed7b5ca75770c12eecdd2dea2
Andreichev Michail <andreichev.m@mail.ru>
2025-05-27 14:34:58 +0300

    Logger update

--- include/Bamboo/Logger.hpp
+++ include/Bamboo/Logger.hpp
@@ -1,24 +1,40 @@
 #pragma once
 
+#ifdef USE_STDFORMAT
 #include <format>
-#include <string>
+#endif
+#include <string_view>
 
 namespace Bamboo {
 
 class Logger {
 public:
     enum MessageType { INFO = 0, WARNING, ERROR };
-    static void log(MessageType type, std::string message);
+    static void log(MessageType type, std::string_view message);
 
+#ifdef USE_STDFORMAT
     template<typename... Args>
-    static void logF(MessageType type, std::format_string<Args...> fstr, Args &&...args) {
-        std::string message = std::format(fstr, std::forward<Args>(args)...);
+    static void log(MessageType type, std::format_string<Args...> fstr, Args &&...args) {
+        std::string_view message = std::format(fstr, std::forward<Args>(args)...);
         log(type, message);
     }
+#else
+    template<typename... Args>
+    static void log(MessageType type, const char *format, ...) {
+        static const int32_t maxSize = 256;
+        static char buffer[maxSize];
+        va_list args;
+        va_start(args, format);
+        vsnprintf(buffer, sizeof(buffer), format, args);
+        va_end(args);
+        std::string_view view = buffer;
+        log(type, view);
+    }
+#endif
 };
 
 } // namespace Bamboo
 
-#define LOG_INFO(...) Bamboo::Logger::logF(Bamboo::Logger::MessageType::INFO, __VA_ARGS__)
-#define LOG_WARN(...) Bamboo::Logger::logF(Bamboo::Logger::MessageType::WARNING, __VA_ARGS__)
-#define LOG_ERROR(...) Bamboo::Logger::logF(Bamboo::Logger::MessageType::ERROR, __VA_ARGS__)
+#define LOG_INFO(...) Bamboo::Logger::log(Bamboo::Logger::MessageType::INFO, __VA_ARGS__)
+#define LOG_WARN(...) Bamboo::Logger::log(Bamboo::Logger::MessageType::WARNING, __VA_ARGS__)
+#define LOG_ERROR(...) Bamboo::Logger::log(Bamboo::Logger::MessageType::ERROR, __VA_ARGS__)
--- src/Bamboo/Logger.cpp
+++ src/Bamboo/Logger.cpp
@@ -1,10 +1,13 @@
 #include "Bamboo/Logger.hpp"
 #include "Panda/OuterScriptHook.hpp"
 
+#include <string>
+
 namespace Bamboo {
 
-void Logger::log(MessageType type, std::string message) {
-    Panda::ExternalCalls::console_Log(type, message.c_str());
+void Logger::log(MessageType type, std::string_view message) {
+    std::string messageStr = {message.begin(), message.end()};
+    Panda::ExternalCalls::console_Log(type, messageStr.c_str());
 }
 
 } // namespace Bamboo
